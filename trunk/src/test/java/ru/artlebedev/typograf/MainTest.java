ab70a63e5b355ddc0f331bb66ab726807e830286
anton.starcev <anton.starcev@f6d3a2b0-2677-11de-9b42-3f9f9954309b>
2009-04-16 18:05:03 +0000

    first test added

---
+++ trunk/src/test/java/ru/artlebedev/typograf/MainTest.java
@@ -0,0 +1,16 @@
+package ru.artlebedev.typograf;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Антон
+ * Date: 12.04.2009
+ * Time: 17:00:20
+ */
+public class MainTest extends TestCase {
+    public void testFirst(){
+        int t = 1;
+        System.out.println("Hello world!");
+    }
+}
