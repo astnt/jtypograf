b644396795fd3728a50d49ea512a51f3eef76e5c
anton.starcev <anton.starcev@f6d3a2b0-2677-11de-9b42-3f9f9954309b>
2009-04-18 14:45:20 +0000

    processor and rules added

---
+++ trunk/src/main/java/ru/artlebedev/typograf/rule/chars/AbstractCharRule.java
@@ -0,0 +1,13 @@
+package ru.artlebedev.typograf.rule.chars;
+
+import ru.artlebedev.typograf.rule.Rule;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Антон
+ * Date: 18.04.2009
+ * Time: 14:35:55
+ */
+public abstract class AbstractCharRule extends Rule {
+    
+}
