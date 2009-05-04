90f38f09f451f0fe978f7938030a808915844767
anton.starcev <anton.starcev@f6d3a2b0-2677-11de-9b42-3f9f9954309b>
2009-05-04 05:45:43 +0000

    pom changed

---
+++ src/main/java/ru/artlebedev/typograf/rule/Rule.java
@@ -0,0 +1,13 @@
+package ru.artlebedev.typograf.rule;
+
+import ru.artlebedev.typograf.Processor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Антон
+ * Date: 18.04.2009
+ * Time: 14:38:34
+ */
+public class Rule {
+    public Processor p;
+}
